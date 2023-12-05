9c17ed08b529b0ba963e9f6ef84be28e11ef5435
Zelin-ff <140753351+Zelin-ff@users.noreply.github.com>
2023-12-05 21:52:19 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cout << "enter an number: " << endl;
+	cin >> num;
+	cout << "your number is: " << num << endl;
+
+	return 0;
+}
+
